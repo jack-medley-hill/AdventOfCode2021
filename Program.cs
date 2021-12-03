ca6f7d744e23623f5e9918d5714f8ccf32c081df
jack-medley-hill <53046387+jack-medley-hill@users.noreply.github.com>
2021-12-03 07:39:14 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
 
-            Console.WriteLine(Result2Part2());
+            Console.WriteLine(Result3Part2());
         }
 
         static int Result1Part1()
@@ -88,6 +88,50 @@ namespace AdventOfCode
                 }
             }
             return x * y;
+        }
+
+        static double Result3Part1()
+        {
+            double gamma = 0;
+            double epsilon = 0;
+            List<string> lines = System.IO.File.ReadAllLines("input3.txt").ToList();
+            int length = lines.Count();
+            for (int i = 0; i < lines[0].Length; i++)
+            {
+                int bit = lines.Select(x => int.Parse(x[i].ToString())).Sum();
+                gamma += bit > length / 2 ? Math.Pow(2, lines[0].Length - 1 - i) : 0;
+                epsilon += bit < length / 2 ? Math.Pow(2, lines[0].Length - 1 - i) : 0;
+            }
+            return gamma * epsilon;
+        }
+
+        static double Result3Part2()
+        {
+            double generator = 0;
+            double scrubber = 0;
+            List<string> generatorLines = System.IO.File.ReadAllLines("input3.txt").ToList();
+            List<string> scrubberLines = System.IO.File.ReadAllLines("input3.txt").ToList();
+            int length = generatorLines[0].Length;
+            for (int i = 0; i < length; i++)
+            {
+
+                int generatorBit = generatorLines.Select(x => int.Parse(x[i].ToString())).Sum();
+                int generatorMostCommon = generatorBit >= generatorLines.Count() / (double)2 ? 1 : 0;
+                generatorLines = generatorLines.Where(x => x[i].ToString() == generatorMostCommon.ToString()).ToList();
+                if (generatorLines.Count == 1)
+                {
+                    generator = Convert.ToUInt64(generatorLines.Single(), 2);
+                }
+
+                int scrubberBit = scrubberLines.Select(x => int.Parse(x[i].ToString())).Sum();
+                int scrubberMostCommon = scrubberBit >= scrubberLines.Count() / (double)2 ? 0 : 1;
+                scrubberLines = scrubberLines.Where(x => x[i].ToString() == scrubberMostCommon.ToString()).ToList();
+                if (scrubberLines.Count == 1)
+                {
+                    scrubber = Convert.ToInt32(scrubberLines.Single(), 2);
+                }
+            }
+            return generator * scrubber;
         }
     }
 }
