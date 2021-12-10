922c19220dd9ee634913d07b86a7e99c1dabc044
jack-medley-hill <53046387+jack-medley-hill@users.noreply.github.com>
2021-12-10 08:55:41 +0000

    Add files via upload

---
+++ Task8.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Task8
+    {
+        public static int Part1()
+        {
+            List<string> lines = System.IO.File.ReadAllLines("input8.txt").ToList();
+            List<List<string>> outputValues = lines.Select(x => x.Split("|")[1]).Select(y => y.Split(" ").ToList().GetRange(1, 4)).ToList();
+            int count = 0;
+            foreach (List<string> outputValue in outputValues)
+            {
+                List<int> uniqueValues = new List<int> { 2, 3, 4, 7 };
+                count += outputValue.Count(x => uniqueValues.Contains(x.Length));
+            }
+            return count;
+        }
+
+        public static int Part2()
+        {
+            List<string> lines = System.IO.File.ReadAllLines("input8.txt").ToList();
+            List<int> results = new List<int>();
+            foreach (string line in lines)
+            {
+                string[] splitLine = line.Split("|");
+                List<string> signalPattern = splitLine[0].Split(" ").ToList().GetRange(0, 10);
+                List<string> outputValues = splitLine[1].Split(" ").ToList().GetRange(1, 4);
+                List<char> oneSignalPattern = signalPattern.Find(x => x.Length == 2).ToList();
+                List<char> fourSignalPattern = signalPattern.Find(x => x.Length == 4).ToList();
+                List<char> sevenSignalPattern = signalPattern.Find(x => x.Length == 3).ToList();
+                List<char> eightSignalPattern = signalPattern.Find(x => x.Length == 7).ToList();
+                List<char> threeSignalPattern = signalPattern.Find(x => x.Length == 5 && sevenSignalPattern.All(y => x.Contains(y))).ToList();
+                List<char> nineSignalPattern = signalPattern.Find(x => x.Length == 6 && fourSignalPattern.All(y => x.Contains(y))).ToList();
+                List<char> fiveSignalPattern = signalPattern.Find(x => x.Length == 5 && !threeSignalPattern.All(y => x.Contains(y)) && x.All(z => nineSignalPattern.Contains(z))).ToList();
+                List<char> sixSignalPattern = signalPattern.Find(x => x.Length == 6 && !nineSignalPattern.All(y => x.Contains(y)) && fiveSignalPattern.All(z => x.Contains(z))).ToList();
+                List<char> twoSignalPattern = signalPattern.Find(x => x.Length == 5 && !threeSignalPattern.All(y => x.Contains(y)) && !fiveSignalPattern.All(z => x.Contains(z))).ToList();
+                List<char> zeroSignalPattern = signalPattern.Find(x => x.Length == 6 && !nineSignalPattern.All(y => x.Contains(y)) && !sixSignalPattern.All(z => x.Contains(z))).ToList();
+                List<string> decodedOutputValues = new List<string>();
+                foreach (string outputValue in outputValues)
+                {
+                    if (outputValue.Length == 2) {
+                        decodedOutputValues.Add("1");
+                    } else if (outputValue.Length == 4)
+                    {
+                        decodedOutputValues.Add("4");
+                    } else if (outputValue.Length == 3)
+                    {
+                        decodedOutputValues.Add("7");
+                    } else if (outputValue.Length == 7)
+                    {
+                        decodedOutputValues.Add("8");
+                    } else if (outputValue.Length == 5)
+                    {
+                        if (outputValue.All(x => threeSignalPattern.Contains(x)))
+                        {
+                            decodedOutputValues.Add("3");
+                        } else if (outputValue.All(x => fiveSignalPattern.Contains(x)))
+                        {
+                            decodedOutputValues.Add("5");
+                        } else if (outputValue.All(x => twoSignalPattern.Contains(x)))
+                        {
+                            decodedOutputValues.Add("2");
+                        }
+                    } else if (outputValue.Length == 6)
+                    {
+                        if (outputValue.All(x => nineSignalPattern.Contains(x)))
+                        {
+                            decodedOutputValues.Add("9");
+                        }
+                        else if (outputValue.All(x => sixSignalPattern.Contains(x)))
+                        {
+                            decodedOutputValues.Add("6");
+                        }
+                        else if (outputValue.All(x => zeroSignalPattern.Contains(x)))
+                        {
+                            decodedOutputValues.Add("0");
+                        }
+                    }
+                }
+                results.Add(int.Parse(string.Join("", decodedOutputValues)));
+            }
+            return results.Sum();
+        }  
+    }
+}
---
+++ Task9.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Task9
+    {
+        public static int Part1()
+        {
+            List<string> lines = System.IO.File.ReadAllLines("input9.txt").ToList();
+            List<List<int>> grid = lines.Select(x => x.ToList().Select(y => int.Parse(y.ToString())).ToList()).ToList();
+            int riskLevels = 0;
+            int gridSize = grid.Count() - 1;
+
+            if (grid[0][0] < grid[0][1] &&
+                grid[0][0] < grid[1][0])
+            {
+                riskLevels += 1 + grid[0][0];
+            }
+
+            if (grid[0][gridSize] < grid[0][gridSize - 1] &&
+                grid[0][gridSize] < grid[1][gridSize])
+            {
+                riskLevels += 1 + grid[0][gridSize];
+            }
+
+            if (grid[gridSize][0] < grid[gridSize][1] &&
+                grid[gridSize][0] < grid[gridSize - 1][0])
+            {
+                riskLevels += 1 + grid[gridSize][0];
+            }
+
+            if (grid[gridSize][gridSize] < grid[gridSize - 1][gridSize] &&
+                grid[gridSize][gridSize] < grid[gridSize][gridSize - 1])
+            {
+                riskLevels += 1 + grid[gridSize][gridSize];
+            }
+
+            for (int i = 1; i < gridSize; i++)
+            {
+                if (grid[i][0] < grid[i - 1][0] &&
+                    grid[i][0] < grid[i + 1][0] &&
+                    grid[i][0] < grid[i][1])
+                {
+                    riskLevels += 1 + grid[i][0];
+                }
+
+                if (grid[0][i] < grid[0][i - 1] &&
+                    grid[0][i] < grid[0][i + 1] &&
+                    grid[0][i] < grid[1][i])
+                {
+                    riskLevels += 1 + grid[0][i];
+                }
+
+                for (int j = 1; j < gridSize; j++)
+                {
+                    if (grid[i][j] < grid[i + 1][j] &&
+                        grid[i][j] < grid[i - 1][j] &&
+                        grid[i][j] < grid[i][j + 1] &&
+                        grid[i][j] < grid[i][j - 1])
+                    {
+                        riskLevels += 1 + grid[i][j];
+                    }
+                }
+
+                if (grid[gridSize][i] < grid[gridSize][i - 1] &&
+                    grid[gridSize][i] < grid[gridSize][i + 1] &&
+                    grid[gridSize][i] < grid[gridSize - 1][i])
+                {
+                    riskLevels += 1 + grid[gridSize][i];
+                }
+
+                if (grid[i][gridSize] < grid[i - 1][gridSize] &&
+                    grid[i][gridSize] < grid[i + 1][gridSize] &&
+                    grid[i][gridSize] < grid[i][gridSize - 1])
+                {
+                    riskLevels += 1 + grid[i][gridSize];
+                }
+            }
+
+            return riskLevels;
+        }
+
+        public static int Part2()
+        {
+            List<string> lines = System.IO.File.ReadAllLines("input9.txt").ToList();
+            List<List<int>> grid = lines.Select(x => x.ToList().Select(y => int.Parse(y.ToString())).ToList()).ToList();
+            int gridSize = grid.Count() - 1;
+            Dictionary<KeyValuePair<int, int>, int> basins = new Dictionary<KeyValuePair<int, int>, int>();
+
+            if (grid[0][0] < grid[0][1] &&
+                grid[0][0] < grid[1][0])
+            {
+                basins.Add(new KeyValuePair<int, int>(0, 0), 0);
+            }
+
+            if (grid[0][gridSize] < grid[0][gridSize - 1] &&
+                grid[0][gridSize] < grid[1][gridSize])
+            {
+                basins.Add(new KeyValuePair<int, int>(0, gridSize), 0);
+            }
+
+            if (grid[gridSize][0] < grid[gridSize][1] &&
+                grid[gridSize][0] < grid[gridSize - 1][0])
+            {
+                basins.Add(new KeyValuePair<int, int>(gridSize, 0), 0);
+            }
+
+            if (grid[gridSize][gridSize] < grid[gridSize - 1][gridSize] &&
+                grid[gridSize][gridSize] < grid[gridSize][gridSize - 1])
+            {
+                basins.Add(new KeyValuePair<int, int>(gridSize, gridSize), 0);
+            }
+
+            for (int i = 1; i < gridSize; i++)
+            {
+                if (grid[i][0] < grid[i - 1][0] &&
+                    grid[i][0] < grid[i + 1][0] &&
+                    grid[i][0] < grid[i][1])
+                {
+                    basins.Add(new KeyValuePair<int, int>(i, 0), 0);
+                }
+
+                if (grid[0][i] < grid[0][i - 1] &&
+                    grid[0][i] < grid[0][i + 1] &&
+                    grid[0][i] < grid[1][i])
+                {
+                    basins.Add(new KeyValuePair<int, int>(0, i), 0);
+                }
+
+                for (int j = 1; j < gridSize; j++)
+                {
+                    if (grid[i][j] < grid[i + 1][j] &&
+                        grid[i][j] < grid[i - 1][j] &&
+                        grid[i][j] < grid[i][j + 1] &&
+                        grid[i][j] < grid[i][j - 1])
+                    {
+                        basins.Add(new KeyValuePair<int, int>(i, j), 0);
+                    }
+                }
+
+                if (grid[gridSize][i] < grid[gridSize][i - 1] &&
+                    grid[gridSize][i] < grid[gridSize][i + 1] &&
+                    grid[gridSize][i] < grid[gridSize - 1][i])
+                {
+                    basins.Add(new KeyValuePair<int, int>(gridSize, i), 0);
+                }
+
+                if (grid[i][gridSize] < grid[i - 1][gridSize] &&
+                    grid[i][gridSize] < grid[i + 1][gridSize] &&
+                    grid[i][gridSize] < grid[i][gridSize - 1])
+                {
+                    basins.Add(new KeyValuePair<int, int>(i, gridSize), 0);
+                }
+            }
+
+            List<KeyValuePair<int, int>> basinsKeys = basins.Keys.ToList();
+
+            foreach (KeyValuePair<int, int> coords in basinsKeys)
+            {
+                List<KeyValuePair<int, int>> currentBasin = new List<KeyValuePair<int, int>>
+                {
+                    coords
+                };
+                List<KeyValuePair<int, int>> currentPass = new List<KeyValuePair<int, int>>
+                {
+                    coords
+                }; 
+                basins[coords]++;
+
+                while (currentPass.Count() != 0)
+                {
+                    List<KeyValuePair<int, int>> nextPass = new List<KeyValuePair<int, int>>();
+
+                    foreach (KeyValuePair<int, int> coord in currentPass)
+                    {
+                        if (coord.Key != 0 && grid[coord.Key - 1][coord.Value] != 9 &&
+                            grid[coord.Key - 1][coord.Value] >= grid[coord.Key][coord.Value])
+                        {
+                            if (!currentBasin.Contains(new KeyValuePair<int, int>(coord.Key - 1, coord.Value)))
+                            {
+                                currentBasin.Add(new KeyValuePair<int, int>(coord.Key - 1, coord.Value));
+                                nextPass.Add(new KeyValuePair<int, int>(coord.Key - 1, coord.Value));
+                                basins[coords]++;
+                            }
+                        }
+
+                        if (coord.Key != 99 && grid[coord.Key + 1][coord.Value] != 9 &&
+                            grid[coord.Key + 1][coord.Value] >= grid[coord.Key][coord.Value])
+                        {
+                            if (!currentBasin.Contains(new KeyValuePair<int, int>(coord.Key + 1, coord.Value)))
+                            {
+
+                                currentBasin.Add(new KeyValuePair<int, int>(coord.Key + 1, coord.Value));
+                                nextPass.Add(new KeyValuePair<int, int>(coord.Key + 1, coord.Value));
+                                basins[coords]++;
+                            }
+                        }
+
+                        if (coord.Value != 0 && grid[coord.Key][coord.Value - 1] != 9 &&
+                            grid[coord.Key][coord.Value - 1] >= grid[coord.Key][coord.Value])
+                        {
+                            if (!currentBasin.Contains(new KeyValuePair<int, int>(coord.Key, coord.Value - 1)))
+                            {
+
+                                currentBasin.Add(new KeyValuePair<int, int>(coord.Key, coord.Value - 1));
+                                nextPass.Add(new KeyValuePair<int, int>(coord.Key, coord.Value - 1));
+                                basins[coords]++;
+                            }
+                        }
+
+                        if (coord.Value != 99 && grid[coord.Key][coord.Value + 1] != 9 &&
+                            grid[coord.Key][coord.Value + 1] >= grid[coord.Key][coord.Value])
+                        {
+                            if (!currentBasin.Contains(new KeyValuePair<int, int>(coord.Key, coord.Value + 1)))
+                            {
+                                currentBasin.Add(new KeyValuePair<int, int>(coord.Key, coord.Value + 1));
+                                nextPass.Add(new KeyValuePair<int, int>(coord.Key, coord.Value + 1));
+                                basins[coords]++;
+                            }
+                        }
+                    }
+
+                    currentPass = nextPass;
+                }
+            }
+
+            return basins.OrderByDescending(x => x.Value).Take(3).Aggregate(1, (acc, value) => acc * value.Value);
+        }
+    }
+}
